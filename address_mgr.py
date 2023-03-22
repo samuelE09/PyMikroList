99ce1cac5b1c259b3b63547102258378220c4be7
samuelE09 <sberrualvarado09@gmail.com>
2023-03-22 13:01:58 -0500

    Se modifica el idioma de ES a EN

--- address_mgr.py
+++ address_mgr.py
@@ -47,8 +47,8 @@ def banner_program():
     "  [+] Program: PyMikroList",
     "  [+] Description: Script to list Move IP addresses between Address List",
     "  [+] Version: 0.1.0",
-    "  [+] Author: Samuel E. Berrú",
-    "  [+] Github: https://github.com/samuelE09/PyMikrolist",
+    "  [+] Author: Samuel E. Berru",
+    #"  [+] Github: https://github.com/samuelE09/PyMikrolist",
   ]
 
   # Variables
@@ -75,13 +75,13 @@ def menu_program():
   from prettytable import PrettyTable
 
   options = {"1":"All Address Lists",
-            "2":"Mover Direcciones IP",
-            "3": "Salir"}
+            "2":"Move IP Addresses",
+            "3": "Exit"}
 
   table = PrettyTable()
   # Añade las columnas de índice y list
-  table.add_column("Opcion", [key for key in options.keys()])
-  table.add_column("Operacion", [value.ljust(len(value)) for value in options.values()])
+  table.add_column("Option", [key for key in options.keys()])
+  table.add_column("Operation", [value.ljust(len(value)) for value in options.values()])
 
   print(table)
 
@@ -97,11 +97,11 @@ def read_file(file):
         data = pd.read_excel(f"{file}")
         info = pd.DataFrame(data)
         info = info["Dirección IP"]
-        msg = "Lectura Satisfactoria"
+        msg = "Successful Reading"
         
     except:
         info = []
-        msg = "No se pudo Leer Archivo"
+        msg = "Could not read file"
 
     return info, msg
 
@@ -121,13 +121,13 @@ def all_address_list(router):
         # Añade las columnas de índice y list
         table.add_column("#", range(1, len(list_count) + 1))
         table.add_column("List", [key for key in list_count.keys()])
-        table.add_column("Direcciones IP", [value for value in list_count.values()])
+        table.add_column("IP Addresses", [value for value in list_count.values()])
         
-        return list_count, table , "Conexion Establecida"
+        return list_count, table , "Connection established"
     
     except:  
         r = [] 
-        return r , "Conexion no Establecida"
+        return r , "Connection not established"
 
 #Pasamos el objeto router que lleva la conexión y ejecutamos los comandos para mover las direcciones IP, de un Address List a Otro, del Equipo 
 def operation_IP(router, to_lista, file):
@@ -144,16 +144,16 @@ def operation_IP(router, to_lista, file):
                     f'/ip firewall address-list add address={ip} list="{to_lista}"')
                 time.sleep(1)
 
-            msg = "Se Movieron Satisfactoriamente"
+            msg = "Moved Succesfull"
         else:  
-            msg = "No hay nada que Mover"
+            msg = "There is nothing to move"
         
         router.close() 
         return msg
     
     except:  
         
-        return "Conexion no Establecida"
+        return "Connection not established"
 
 
 if __name__ == "__main__":
@@ -179,8 +179,8 @@ if __name__ == "__main__":
             
             if option_selected == "1":
                 data, respuesta, msg = all_address_list(r1.conexion_ros_api())
-                print(msg)
-                print(respuesta)
+                print(f"\n {msg}")
+                print(f"\n {respuesta}")
                 
             elif option_selected == "2":
                 
@@ -191,33 +191,33 @@ if __name__ == "__main__":
                 # Añade las columnas de índice y list
                 table.add_column("#", range(1, len(data) + 1))
                 table.add_column("List", [key for key in data.keys()])
-                print(table)
+                print(f"\n {table}")
                 
                 while True: 
                     
-                    opcion = int(input("\n [*] A que lista quieres Mover las IP: "))
+                    opcion = int(input("\n [*] To which list do you want to move the IPs: "))
                         
                     if opcion in range(1, len(opciones)+1):
                         lista_seleccionada = opciones[opcion-1]
                         msg = operation_IP(r1.conexion_paramiko(),lista_seleccionada, file)
-                        print(msg)
+                        print(f"\n {msg}")
                         break
                     else:
-                        print("\n [Warning!] Opción no válida. Por favor seleccione una opción válida.")
+                        print("\n [Warning!] Invalid option. Please select a valid option.")
                         
             elif option_selected == "3":
-                print(f"Saliendo del Programa ......! ")
+                print(f"\nExiting the Program ......! ")
                 time.sleep(3)
                 exit()
         else: 
             print("\n [Warning!] Run the program again and select a valid option.")
             
     except KeyboardInterrupt:
-        print("\nPrograma interrumpido por el usuario. Saliendo.....!")
+        print("\n Program interrupted by the user. Exiting.....!")
         time.sleep(3)
         exit()
     
     except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")   
+        print(f"\n Unexpected {err=}, {type(err)=}")   
 
     
